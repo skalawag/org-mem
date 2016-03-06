8334fe02a75ab9afe5b1008f9277248aa8c8b944
Mark Scala <markscala@gmail.com>
2016-03-05 19:30:22 -0600

    removed utils.el

--- utils.el
+++
@@ -1,9 +0,0 @@
-(defun shuffle (LIST)
-  "Shuffle the elements in LIST.
-shuffling is done in place."
-  (loop for i in (reverse (number-sequence 1 (1- (length LIST))))
-        do (let ((j (random (+ i 1)))
-		 (tmp (elt LIST i)))
-	     (setf (elt LIST i) (elt LIST j))
-	     (setf (elt LIST j) tmp)))
-  LIST)
